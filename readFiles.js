4b7d68b904d5ec35478e3dfc9659db01c6c23b7f
Paige Niedringhaus <paige_e_niedringhaus@homedepot.com>
2018-10-04 10:26:34 -0400

    finished up with the donation counts per month

--- readFiles.js
+++ readFiles.js
@@ -12,7 +12,9 @@ var lineCount = 0;
 // create array list of names
 var names = [];
 
-// donations occurring in each month (date parsing library allowed)
+// donations occurring in each month
+var dateDonationCount = [];
+var dateDonations = {};
 
 // list of first names, and most common first name
 var firstNames = [];
@@ -39,6 +41,11 @@ rl.on('line', function(line) {
     firstNames.push(firstHalfOfName);
   }
   firstNames.push(firstHalfOfName);
+
+  // year and month
+  var timestamp = line.split('|')[4].slice(0, 6);
+  var formattedTimestamp = timestamp.slice(0, 4) + '-' + timestamp.slice(4, 6);
+  dateDonationCount.push(formattedTimestamp);
 });
 
 rl.on('close', function() {
@@ -53,16 +60,23 @@ rl.on('close', function() {
   firstNames.forEach(x => {
     dupeNames[x] = (dupeNames[x] || 0) + 1;
   });
-
   var sortedDupeNames = [];
-
   for (var name in dupeNames) {
     sortedDupeNames.push([name, dupeNames[name]]);
   }
-
   sortedDupeNames.sort((a, b) => {
     return a[1] - b[1];
   });
-
   console.log(sortedDupeNames[sortedDupeNames.length - 1]);
+
+  // number of donations per month
+  dateDonationCount.forEach(x => {
+    dateDonations[x] = (dateDonations[x] || 0) + 1;
+  });
+  logDateElements = (key, value, map) => {
+    console.log(
+      `Donations per month and year: ${value} and donation count ${key}`,
+    );
+  };
+  new Map(Object.entries(dateDonations)).forEach(logDateElements);
 });
