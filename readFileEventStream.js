6acbb458deb6fc0a07438720bbb0ba3685ddf14e
Paige Niedringhaus <paige_e_niedringhaus@homedepot.com>
2018-10-08 21:52:53 -0400

    reworking streams

--- readFileEventStream.js
+++ readFileEventStream.js
@@ -10,9 +10,9 @@ var dateDonationCount = [];
 var dateDonations = {};
 
 var s = fs
-  .createReadStream('test.txt')
+  // .createReadStream('test.txt')
   // .createReadStream('itcont.txt')
-  // .createReadStream('/Users/pxn5096/Downloads/indiv18/itcont.txt')
+  .createReadStream('/Users/pxn5096/Downloads/indiv18/itcont.txt')
   .pipe(es.split())
   .pipe(
     es
@@ -23,7 +23,9 @@ var s = fs
         // get all names
         console.time('names');
         var name = line.split('|')[7];
-        names.push(name);
+        if (totalLines === 432 || totalLines === 43243) {
+          names.push(name);
+        }
 
         // get all first halves of names
         console.time('most common first name');
@@ -35,6 +37,17 @@ var s = fs
           firstNames.push(firstHalfOfName);
         }
         firstNames.push(firstHalfOfName);
+
+        firstNames.forEach(x => {
+          dupeNames[x] = (dupeNames[x] || 0) + 1;
+        });
+        var sortedDupeNames = [];
+        for (var name in dupeNames) {
+          sortedDupeNames.push([name, dupeNames[name]]);
+        }
+        sortedDupeNames.sort((a, b) => {
+          return a[1] - b[1];
+        });
 
         // year and month
         console.time('total donations for each month');
@@ -50,5 +63,24 @@ var s = fs
         console.log('Read entire file.');
         console.log(totalLines);
         console.timeEnd('line count');
+
+        console.log(names);
+        console.timeEnd('names');
+
+        // most common first name
+        console.log(sortedDupeNames[sortedDupeNames.length - 1]);
+        console.timeEnd('most common first name');
+
+        // number of donations per month
+        dateDonationCount.forEach(x => {
+          dateDonations[x] = (dateDonations[x] || 0) + 1;
+        });
+        logDateElements = (key, value, map) => {
+          console.log(
+            `Donations per month and year: ${value} and donation count ${key}`,
+          );
+        };
+        new Map(Object.entries(dateDonations)).forEach(logDateElements);
+        console.timeEnd('total donations for each month');
       }),
   );
