c6f014e5df636c30ee74c14fa78d6f06c1fe47e6
Paige Niedringhaus <paige_e_niedringhaus@homedepot.com>
2018-10-13 11:38:30 -0400

    adding additional performance testing package

--- readFile.js
+++ readFile.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
+var now = require('performance-now');
 
 var totalLines = 0;
 var lines = [];
@@ -10,13 +11,17 @@ var dateDonations = {};
 
 fs.readFile('itcont.txt', 'utf8', (err, contents) => {
   console.time('line count');
+  let t0 = now();
   if (contents !== undefined) {
     totalLines = contents.split('\n').length - 1;
   }
   console.log(totalLines);
+  let t1 = now();
   console.timeEnd('line count');
+  console.log(`Performance now line count timing: ` + (t1 - t0).toFixed(3));
 
   console.time('names');
+  t0 = now();
   if (contents !== undefined) {
     lines = contents.split('\n');
     lines.forEach(line => {
@@ -27,9 +32,12 @@ fs.readFile('itcont.txt', 'utf8', (err, contents) => {
 
   console.log(names[432]);
   console.log(names[43243]);
+  t1 = now();
   console.timeEnd('names');
+  console.log(`Performance now names timing: ` + (t1 - t0).toFixed(3));
 
   console.time('most common first name');
+  t0 = now();
   names.forEach(name => {
     var firstHalfOfName = name.split(', ')[1];
     if (firstHalfOfName !== undefined) {
@@ -55,9 +63,12 @@ fs.readFile('itcont.txt', 'utf8', (err, contents) => {
     return b[1] - a[1];
   });
   console.log(sortedDupeNames[0]);
+  t1 = now();
   console.timeEnd('most common first name');
+  console.log(`Performance now first name timing: ` + (t1 - t0).toFixed(3));
 
   console.time('total donations for each month');
+  t0 = now();
   lines.forEach(line => {
     var timestamp = line.split('|')[4].slice(0, 6);
     var formattedTimestamp =
@@ -74,6 +85,9 @@ fs.readFile('itcont.txt', 'utf8', (err, contents) => {
     );
   };
   new Map(Object.entries(dateDonations)).forEach(logDateElements);
-
+  t1 = now();
   console.timeEnd('total donations for each month');
+  console.log(
+    `Performance now donations per month timing: ` + (t1 - t0).toFixed(3),
+  );
 });
