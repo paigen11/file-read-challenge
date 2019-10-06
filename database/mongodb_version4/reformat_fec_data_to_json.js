f14083a1b2e1b4fb0decbb3550feffb1b379c372
BobCochran <r2cochran2@gmail.com>
2019-10-06 19:24:42 -0400

    Build the json output.

--- database/mongodb_version4/reformat_fec_data_to_json.js
+++ database/mongodb_version4/reformat_fec_data_to_json.js
@@ -63,7 +63,7 @@ rl.on('line', (line) => {
 
 	var jstring = "{ "
 
-	for (i = 0; i < 7; i++) {
+	for (i = 0; i < 13; i++) {
 
 		jstring = jstring + "\"" + myHdr[i] + "\" : " + "\"" + myTrans[i] + "\"\, "
 
